e025ec039610d2229fb4eccf9a220f206e6e51a0
JAREDMZV <147438805+JAREDMZV@users.noreply.github.com>
2023-12-04 23:20:35 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
-var div = document.getElementById('mi-div');
+var miOtroDiv = document.getElementById('mi-otro-div');
 
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+anime({
+    targets: '#mi-otro-div',
+    translateX: [
+        { value: 250, duration: 1000 },
+        { value: 0, duration: 800 }
+    ],
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 2000,
+    loop: false // Para una sola vuelta de la animación
+});
+
+var div = document.getElementById('mi-div');
+var count = 0;
 
 function animateDiv() {
-    if (isElementInViewport(div)) {
+    if (count < 10) {
+        var windowWidth = window.innerWidth;
+        var divWidth = div.offsetWidth;
         anime({
             targets: '#mi-div',
             translateX: [
-                { value: 250, duration: 1000 },
-                { value: 0, duration: 800 }
+                { value: windowWidth - divWidth, duration: 2000, easing: 'linear' }, // Mueve hacia la derecha
+                { value: 0, duration: 2000, easing: 'linear' } // Mueve hacia la izquierda
             ],
-            rotate: '1turn',
-            backgroundColor: '#FFF',
-            duration: 2000,
-            loop: false // Para una sola vuelta de la animación
+            borderRadius: 50,
+            loop: true,
+            complete: function(anim) {
+                count++;
+                if (count >= 10) {
+                    anim.pause(); // Pausa la animación después de 10 repeticiones
+                }
+            }
         });
-        window.removeEventListener('scroll', animateDiv);
-        window.removeEventListener('touchmove', animateDiv); // Elimina el evento touchmove después de la animación
     }
 }
 
-window.addEventListener('scroll', animateDiv);
-window.addEventListener('touchmove', animateDiv); // Agrega el evento touchmove para dispositivos táctiles
+animateDiv(); // Inicia la animación
